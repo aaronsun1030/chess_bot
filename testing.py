f06291cc71d54f0e144de426ccf995539ce86b62
Aaron Sun <aaronsun2000@berkeley.edu>
2020-10-06 13:19:25 -0700

    ready to commit

--- testing.py
+++ testing.py
@@ -5,33 +5,44 @@ import chess
 import time
 
 pgn = open("testing/tactics.pgn")
-failed = open("testing/failed.txt", 'w')
+failed = open("testing/failed.txt", 'a')
+current = open("testing/current.txt", 'r')
 h = heuristic.heuristic()
 white = AI.AI(None, 1, h)
 black = AI.AI(None, -1, h)
 
 tactic = chess.pgn.read_game(pgn)
+first_try = True
 fail = False
 last_d = 0
+num = int(current.read())
+current.close()
+current = open("testing/current.txt", 'w')
+current.write(str(num + 10))
+current.close()
+
+for i in range(num):
+    tactic = chess.pgn.read_game(pgn)
+
 num = 0
 
 while tactic:
-    player = white if tactic.headers._tag_roster['White'] == 'solver' else black
-    player.board = tactic.board()
-    turn = 0
-    if not fail:
+    fail = False
+    if first_try:
+        player = white if tactic.headers._tag_roster['White'] == 'solver' else black
         player.d_limit = 1
         last_d = player.d_limit
+        player.board = tactic.board()
+        turn = 0
     else:
         player.d_limit = last_d + 1
         last_d += 1
-        fail = False
     start = time.time()
     for move in tactic.mainline_moves():
         if turn % 2 != 0:
             m = player.best_move()
             if m != move:
-                if not fail:
+                if first_try:
                     failed.write(str(tactic)[:-11] + '\n')
                 failed.write("Played move " + str(m) + " instead of " + str(move) + '\n')
                 fail = True
@@ -42,11 +53,12 @@ while tactic:
         failed.write("Success at depth " + str(last_d) + " in " + str(time.time() - start) + " seconds for the last iteration.\n\n")
         tactic = chess.pgn.read_game(pgn)
         num += 1
-        if num % 100 == 0:
-            failed.close()
-            failed = open("testing/failed.txt", 'a')
+        if num % 10 == 0:
+            break
+        first_try = True
     else:
         failed.write('Failed at depth ' + str(last_d) + " in " + str(time.time() - start) + ' seconds. Trying again at a higher depth.\n')
+        first_try = False
 
 pgn.close()
 failed.close()
